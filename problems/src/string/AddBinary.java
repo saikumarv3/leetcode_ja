bd283bf065d01331d612fb59512885d31c1c5be0
Sai Kumar Vurukonda <54095747+Saikumarvurukonda@users.noreply.github.com>
2021-06-23 22:42:50 -0500

    string

--- problems/src/string/AddBinary.java
+++ problems/src/string/AddBinary.java
@@ -1,8 +1,6 @@
 package string;
 
 /**
- * Created by gouthamvidyapradhan on 25/11/2017.
- *
  * <p>Given two binary strings, return their sum (also a binary string).
  *
  * <p>For example, a = "11" b = "1" Return "100".
