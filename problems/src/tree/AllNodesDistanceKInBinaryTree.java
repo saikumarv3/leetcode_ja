322ff1cde556833a2a5272226455884e424b8e10
Sai Kumar Vurukonda <54095747+Saikumarvurukonda@users.noreply.github.com>
2021-08-22 11:31:34 -0400

    tree

--- problems/src/tree/AllNodesDistanceKInBinaryTree.java
+++ problems/src/tree/AllNodesDistanceKInBinaryTree.java
@@ -3,7 +3,7 @@ package tree;
 import java.util.*;
 
 /**
- * Created by gouthamvidyapradhan on 26/04/2019 We are given a binary tree (with root node root), a
+ * We are given a binary tree (with root node root), a
  * target node, and an integer value K.
  *
  * <p>Return a list of the values of all nodes that have a distance K from the target node. The
